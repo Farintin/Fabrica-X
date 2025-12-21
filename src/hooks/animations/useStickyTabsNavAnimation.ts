70d2572d5cf5c8d949dafe8c929ba031d56607f9
farintin <farintinobialor@gmail.com>
2025-12-21 17:08:27 +0100

    remove unneeded commented out animations style logic

--- src/hooks/animations/useStickyTabsNavAnimation.ts
+++ src/hooks/animations/useStickyTabsNavAnimation.ts
@@ -13,11 +13,6 @@ export function useStickyTabsNavAnimation(isSticky: boolean) {
   }, [isSticky]);
 
   return useAnimatedStyle(() => ({
-    // opacity: withTiming(isSticky ? 1 : 0, { duration: 500 }),
-    // transform: [
-    //   { translateY: withTiming(isSticky ? 0 : 8, { duration: 500 }) },
-    //   { scale: withTiming(isSticky ? 1 : 0.96, { duration: 500 }) },
-    // ],
     opacity: stickyOpacity.value,
     transform: [{ translateY: stickyOpacity.value * -10 }],
   }));
